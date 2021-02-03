ade7ab34952a5789dbdbc45cc142cff065f315b3
RedCreator37 <tobija.zuntar@gmail.com>
2021-02-03 19:41:04 +0100

    [WIP] Major code cleanup in Grabber.java

--- src/main/java/com/redcreator37/ImageFetcherBot/Grabber.java
+++ src/main/java/com/redcreator37/ImageFetcherBot/Grabber.java
@@ -3,9 +3,9 @@ package com.redcreator37.ImageFetcherBot;
 import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Attachment;
-import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.rest.util.Color;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.BufferedWriter;
@@ -19,12 +19,11 @@ import java.nio.channels.ReadableByteChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 @SuppressWarnings("BlockingMethodInNonBlockingContext")
 public class Grabber {
+
+    private static final String LINK_OUTPUT = "output.txt";
 
     public static Mono<Void> grabFiles(MessageCreateEvent event) {
         MessageChannel channel = event.getMessage().getChannel().block();
@@ -40,7 +39,7 @@ public class Grabber {
         String[] params = event.getMessage().getContent().split(" ");
         downloadFiles(channel, params.length == 1
                 ? Snowflake.of(Instant.now())
-                : Snowflake.of(params[1])).block();
+                : Snowflake.of(params[1]));
 
         channel.createEmbed(spec -> {
             spec.setTitle("Done!");
@@ -62,7 +61,14 @@ public class Grabber {
                     "You will be notified once the process has been completed", false);
             spec.setTimestamp(Instant.now());
         }).block();
-        grabAll(channel).block();
+
+        String[] params = event.getMessage().getContent().split(" ");
+        downloadFiles(channel, params.length == 1
+                ? Snowflake.of(Instant.now())
+                : Snowflake.of(params[1]));
+
+        Snowflake before = Snowflake.of(params[1]);
+        saveUrls(grabAttachments(channel, before));
         channel.createEmbed(spec -> {
             spec.setTitle("Done!");
             spec.setColor(Color.GREEN);
@@ -73,53 +79,45 @@ public class Grabber {
         return Mono.empty();
     }
 
-    private static Mono<Void> grabAll(MessageChannel channel) {
-        List<Message> messages = channel.getMessagesBefore(Snowflake
-                .of(Instant.now())).collectList().block();
-
-        List<Attachment> attachments = new ArrayList<>();
-        Objects.requireNonNull(messages).forEach(message ->
-                attachments.addAll(message.getAttachments()));
-
-        return saveUrls(Objects.requireNonNull(attachments));
+    private static Flux<Attachment> grabAttachments(MessageChannel channel, Snowflake before) {
+        return channel.getMessagesBefore(before)
+                .flatMap(message -> Flux.fromIterable(message.getAttachments()));
     }
 
-    private static Mono<Void> saveUrls(Iterable<Attachment> attachments) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
-            for (Attachment a : attachments)
+    private static void saveUrls(Flux<Attachment> attachments) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LINK_OUTPUT))) {
+            for (Attachment a : attachments.toIterable())
                 writer.write(a.getUrl() + "\n");
         } catch (IOException e) {
             System.err.println("Saving data failed: " + e.getMessage());
         }
-        return Mono.empty();
     }
 
-    private static Mono<Void> downloadFiles(MessageChannel channel, Snowflake messagesBefore) {
-        List<Message> messages = channel.getMessagesBefore(messagesBefore)
-                .collectList().block();
+    private static void downloadFiles(MessageChannel channel, Snowflake before) {
+        if (!makeDirIfNotExists(new File("retrieved")))
+            return;
+        grabAttachments(channel, before).doOnNext(Grabber::downloadAttachment);
+    }
 
-        List<Attachment> attachments = new ArrayList<>();
-        Objects.requireNonNull(messages).forEach(message ->
-                attachments.addAll(message.getAttachments()));
+    private static boolean makeDirIfNotExists(File dir) {
+        return dir.exists() || dir.mkdir();
+    }
 
-        if (!new File("retrieved").mkdir()) return Mono.empty();
-        attachments.forEach(attachment -> {
-            try {
-                ReadableByteChannel readableByteChannel = Channels
-                        .newChannel(new URL(attachment.getUrl()).openStream());
+    private static void downloadAttachment(Attachment attachment) {
+        try {
+            ReadableByteChannel readableByteChannel = Channels
+                    .newChannel(new URL(attachment.getUrl()).openStream());
 
-                // the following prefixes message snowflakes to avoid filename collisions
-                Path outPath = Paths.get("retrieved", attachment.getId().toString()
-                        + "_" + attachment.getFilename());
+            // prefix filenames with snowflakes to avoid collisions
+            Path outPath = Paths.get("retrieved", attachment.getId().toString()
+                    + "_" + attachment.getFilename());
 
-                FileOutputStream fileOutputStream = new FileOutputStream(outPath.toFile());
-                fileOutputStream.getChannel()
-                        .transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-            } catch (IOException e) {   // todo: better error and progress reporting
-                System.err.println(e.getMessage());
-            }
-        });
-        return saveUrls(Objects.requireNonNull(attachments));
+            FileOutputStream fileOutputStream = new FileOutputStream(outPath.toFile());
+            fileOutputStream.getChannel()
+                    .transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+        } catch (IOException e) {   // todo: better error and progress reporting
+            System.err.println(e.getMessage());
+        }
     }
 
 }
