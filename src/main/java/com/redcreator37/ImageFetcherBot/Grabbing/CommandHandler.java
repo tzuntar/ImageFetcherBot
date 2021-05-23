cf6a6c4c5fe1bbc9d1f08a29e1e6716b3f041410
RedCreator37 <tobija.zuntar@gmail.com>
2021-05-23 14:08:58 +0200

    [WIP] Merge the parameter validation code into its own function

--- src/main/java/com/redcreator37/ImageFetcherBot/Grabbing/CommandHandler.java
+++ src/main/java/com/redcreator37/ImageFetcherBot/Grabbing/CommandHandler.java
@@ -5,6 +5,10 @@ import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import java.util.Objects;
 
 /**
  * Manages general command handling and redirection
@@ -21,28 +25,17 @@ public class CommandHandler {
      * @return an empty {@link Mono}
      */
     public static Mono<Void> grabFiles(MessageCreateEvent event) {
-        MessageChannel channel = event.getMessage().getChannel().block();
-        assert channel != null;
-        String[] params = event.getMessage().getContent().split(" ");
-        if (params.length > 1 && !params[1].matches("before|after")) {
-            return channel.createEmbed(spec ->
-                    ProgressEmbeds.invalidSyntax(spec, true)).then();
-        }
+        Tuple2<MessageChannel, String[]> optChannel = checkGrabberParams(event, true);
+        if (optChannel == null) return Mono.empty();
+        MessageChannel channel = optChannel.getT1();
 
         Message progressMsg = channel.createEmbed(spec ->
                 ProgressEmbeds.progressEmbed(spec, 1, 1)).block();
-        Grabber grabber = new Grabber(new OutputDefinition("retrieved",
+        assert progressMsg != null;
+        String folderName = Objects.requireNonNull(progressMsg.getGuild().block()).getName();
+        Grabber grabber = new Grabber(new OutputDefinition(folderName,
                 OutputDefinition.Type.FILE_DIRECTORY), progressMsg);
-
-        if (params.length < 2) {   // no parameters, get everything
-            grabber.grabBefore(channel, null);
-        } else if (params.length == 3) {    // before / after ID
-            if (params[1].equalsIgnoreCase("before"))
-                grabber.grabBefore(channel, Snowflake.of(params[2]));
-            else if (params[1].equalsIgnoreCase("after"))
-                grabber.grabAfter(channel, Snowflake.of(params[2]));
-        }
-
+        grabFromParams(grabber, channel, optChannel.getT2());
         return channel.createEmbed(ProgressEmbeds::retrievingFilesFinished).then();
     }
 
@@ -54,29 +47,56 @@ public class CommandHandler {
      * @return an empty {@link Mono}
      */
     public static Mono<Void> grabLinks(MessageCreateEvent event) {
-        MessageChannel channel = event.getMessage().getChannel().block();
-        assert channel != null;
-        String[] params = event.getMessage().getContent().split(" ");
-        if (params.length > 1 && !params[1].matches("before|after")) {
-            return channel.createEmbed(spec ->
-                    ProgressEmbeds.invalidSyntax(spec, true)).then();
-        }
+        Tuple2<MessageChannel, String[]> optChannel = checkGrabberParams(event, false);
+        if (optChannel == null) return Mono.empty();
+        MessageChannel channel = optChannel.getT1();
 
         Message progressMsg = channel.createEmbed(spec ->
                 ProgressEmbeds.progressEmbed(spec, 1, 1)).block();
         Grabber grabber = new Grabber(new OutputDefinition("output.txt",
                 OutputDefinition.Type.LINK_FILE), progressMsg);
+        grabFromParams(grabber, channel, optChannel.getT2());
+        return channel.createEmbed(ProgressEmbeds::savingLinksFinished).then();
+    }
 
-        if (params.length < 2) {   // no parameters, get everything
+    /**
+     * Checks whether the grab command params are valid
+     *
+     * @param event         the {@link MessageCreateEvent} of the message
+     * @param grabbingFiles {@code true} if grabbing files instead of
+     *                      plain links
+     * @return the associated message's {@link MessageChannel} and an
+     * array of string parameters, passed to the command if the
+     * syntax is correct. {@code null} is returned on incorrect syntax.
+     */
+    private static Tuple2<MessageChannel, String[]> checkGrabberParams(MessageCreateEvent event, boolean grabbingFiles) {
+        MessageChannel channel = event.getMessage().getChannel().block();
+        assert channel != null;
+        String[] params = event.getMessage().getContent().split(" ");
+        if (params.length > 1 && !params[1].matches("before|after")) {
+            channel.createEmbed(spec ->
+                    ProgressEmbeds.invalidSyntax(spec, grabbingFiles));
+            return null;
+        }
+        return Tuples.of(channel, params);
+    }
+
+    /**
+     * Parses the command parameters and runs the appropriate grabber
+     *
+     * @param grabber the {@link Grabber} with all options set
+     * @param channel the {@link MessageChannel} where the command message
+     *                has been posted
+     * @param params  parameters to the command
+     */
+    private static void grabFromParams(Grabber grabber, MessageChannel channel, String[] params) {
+        if (params.length < 2)   // no parameters, get everything
             grabber.grabBefore(channel, null);
-        } else if (params.length == 3) {    // before / after ID
+        else if (params.length == 3)    // before / after ID
             if (params[1].equalsIgnoreCase("before"))
                 grabber.grabBefore(channel, Snowflake.of(params[2]));
             else if (params[1].equalsIgnoreCase("after"))
                 grabber.grabAfter(channel, Snowflake.of(params[2]));
-        }
-
-        return channel.createEmbed(ProgressEmbeds::savingLinksFinished).then();
     }
 
 }
